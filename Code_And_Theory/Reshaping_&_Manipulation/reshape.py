9de2b0e8088d58f7d1e78752a37c56fcb5d8effb
Suraj Kumar <surajkumar09464@gmail.com>
2026-01-23 01:09:19 +0530

    add

---
+++ Code_And_Theory/Reshaping_&_Manipulation/reshape.py
@@ -0,0 +1,9 @@
+"""
+reshape(row, columns) specify new shape 
+if dimensions match
+"""
+
+import numpy as np
+arr = np.array([10,20,30,40,50,60])
+reshaped_arr = arr.reshape(2, 3)
+print(reshaped_arr)
